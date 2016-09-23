9ca1473871e6e8f699f25f305c96e9f50eabfc90
vamsivicky001 <vamsivicky001@gmail.com>
2016-09-23 12:08:12 +0530

    A new line of code is added to Main.java in local repository

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+	public static void main(String... args)
+	{
+		System.out.println("Print statement from local repository");
+		System.out.println("The Main.java file is modified in local repository")
+	}
+}
