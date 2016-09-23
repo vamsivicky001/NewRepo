1e93221a41f00a7b688d858a32536c5f87a029a5
vamsivicky001 <vamsivicky001@gmail.com>
2016-09-23 12:17:11 +0530

    A new line of code is added in Remote repository

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main
 	public static void main(String... args)
 	{
 		System.out.println("Print statement from local repository");
-		System.out.println("The Main.java file is modified in local repository")
+		System.out.println("The Main.java file is modified in Local repository");
+		System.out.println("The Main.java file is modified in Remote repository");
 	}
 }
